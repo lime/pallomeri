7a74df5068f2fbdb50b1ade940d5a1d1fa9a368b
Emil S <emil.sagfors@aalto.fi>
2012-10-24 22:41:51 +0300

    Valikko-rakenne toimimaan

--- src/pallomeri/Pallomeri.java
+++ src/pallomeri/Pallomeri.java
@@ -1,25 +1,45 @@
 package pallomeri;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import processing.core.PApplet;
+import processing.core.PGraphics;
 
 public class Pallomeri extends PApplet {
 
 	private Set<Pallo> pallot;
+	private Valikko valikko;
+	private static final int STAGE_HEIGHT = 500;
+	private static final int VALIKKO_HEIGHT = 100;
 
 	public void setup() {
+		this.size(800, STAGE_HEIGHT + VALIKKO_HEIGHT);
+		
 		String kuvanNimi = null; // TODO hoida tämä
+		
+		this.pallot = new HashSet<Pallo>();
+		
 		// Luodaan uusi Kuvanlukija
 		Kuvanlukija lukija = new Kuvanlukija(kuvanNimi, this);
 
 		for (int x = 0; x < lukija.annaLeveys(); x++) {
 			for (int y = 0; y < lukija.annaKorkeus(); y++) {
-				pallot.add(lukija.luePikseli(x, y));
+				this.pallot.add(lukija.luePikseli(x, y));
 			}
 		}
+		
+		// Varsinaista kikkailua jolla saamme valikkoa käyttämään PGraphics:ia
+		this.valikko = new Valikko(this.width, VALIKKO_HEIGHT, this);
 	}
 
 	public void draw() {
+		
+		// Piirrä tausta
+		this.background(this.color(32, 38, 39));
+		
+		// Päivitä valikko ja piirrä se näytölle
+		this.valikko.draw();
+		this.image(valikko.getGraphics(), 0.0f, this.height - valikko.height);
 	}
 }
---
+++ src/pallomeri/Valikko.java
@@ -0,0 +1,37 @@
+package pallomeri;
+
+import processing.core.PApplet;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+import processing.core.PGraphicsJava2D;
+import processing.core.PImage;
+
+public class Valikko {
+	
+	private PGraphics g;
+	private PApplet applet;
+	final float width, height;
+	
+	public Valikko(int width, int height, PApplet applet) {
+		this.width = width;
+		this.height = height;
+		this.applet = applet;
+		g = applet.createGraphics(width, height, PConstants.JAVA2D);
+	}
+
+	/**
+	 * Tässä metodissa voi tehdä muutoksia valikolle
+	 */
+	public void draw() {
+		g.beginDraw(); // aloita
+
+		g.background(applet.color(58, 63, 64));
+		
+
+		g.endDraw(); // lopeta
+	}
+
+	public PImage getGraphics() {
+		return g;
+	}
+}
