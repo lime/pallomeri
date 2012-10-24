a7630a00df6498526f4c973f2d8233c58deec474
Emil S <emil.sagfors@aalto.fi>
2012-10-24 16:10:13 +0300

    use the Kuvanlukija to read each pixel into a Pallo

--- src/pallomeri/Pallomeri.java
+++ src/pallomeri/Pallomeri.java
@@ -1,11 +1,23 @@
 package pallomeri;
 
-import processing.core.PApplet;
+import java.util.Set;
 
+import processing.core.PApplet;
 
 public class Pallomeri extends PApplet {
 
+	private Set<Pallo> pallot;
+
 	public void setup() {
+		String kuvanNimi = null; // TODO hoida tämä
+		// Luodaan uusi Kuvanlukija
+		Kuvanlukija lukija = new Kuvanlukija(kuvanNimi, this);
+
+		for (int x = 0; x < lukija.annaLeveys(); x++) {
+			for (int y = 0; y < lukija.annaKorkeus(); y++) {
+				pallot.add(lukija.luePikseli(x, y));
+			}
+		}
 	}
 
 	public void draw() {
