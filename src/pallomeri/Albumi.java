78fea1e1fb3e2256a9a844139b51c995d611fa94
viivi <viivi.kerri@gmail.com>
2012-11-02 11:33:46 +0200

    lisää kuvia

--- src/pallomeri/Albumi.java
+++ src/pallomeri/Albumi.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public enum Albumi {
 
-	MONALISA, URKKI, NELSON, TOMATSOPPA, YODA;
+	MONALISA, URKKI, NELSON, TOMATSOPPA, YODA, AKU, APPLE, NEMO, HOMER, HUUTO, JUDELAW, KORALLI, PABLO;
 	
 	public String annakuvannimi(){
 		
@@ -15,6 +15,14 @@ public enum Albumi {
 		case NELSON: s="data/nelson.jpg"; break;
 		case TOMATSOPPA: s="data/tomatsoppa.png"; break;
 		case YODA: s="data/yoda.jpg"; break;
+		case  AKU: s = "data/aku.jpg"; break;
+		case APPLE: s= "data/apple.jpg"; break;
+		case NEMO: s = "data/nemo.jpg"; break;
+		case HOMER: s = "data/homer.jpg"; break;
+		case HUUTO: s = "data/huuto.jpg"; break;
+		case JUDELAW: s = "data/judelaw.jpg"; break;
+		case KORALLI: s = "data/koralli.jpg"; break;
+		case PABLO: s = "data/pablo.jpg"; break;
 		default: s="data/urho.jpg"; break;
 		}
 		return s;
